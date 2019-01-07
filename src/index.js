6db6978d0c1ad9f258cafd2b13ea15ddf2e0c6c0
Kokoster <dokokoster9@gmail.com>
2019-01-07 15:31:30 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+﻿const express = require('express')
+
+const app = express()
+const port = 8009
+
+app.get('/', (req, res) => {
+	res.send('Hello world!')
+})
+
+app.listen(port, (err) => {
+	if (err) {
+		return console.log('Failed: ' + err)
+	}
+
+	console.log('Server is listening on port: ' + port)
+})
